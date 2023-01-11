452764669aa417217cff6af281f73b941ae6f292
serhii.nastenko <serhii.nastenko@nentgroup.com>
2023-01-12 00:29:37 +0100

    Created actors skeletons

--- VampusWorld.scala
+++ VampusWorld.scala
@@ -5,40 +5,11 @@ case object PongMessage
 case object StartMessage
 case object StopMessage
 
-class Ping(pong: ActorRef) extends Actor {
-  var count = 0
-  def incrementAndPrint = { count += 1; println("ping") }
-  def receive = {
-    case StartMessage =>
-        incrementAndPrint
-        pong ! PingMessage
-    case PongMessage => 
-        incrementAndPrint
-        if (count > 99) {
-          sender() ! StopMessage
-          println("ping stopped")
-          context.stop(self)
-        } else {
-          sender() ! PingMessage
-        }
-  }
-}
+class Speleologist(navigator: ActorRef) extends Actor {}
+class Navigator extends Actor {}
 
-class Pong extends Actor {
-  def receive = {
-    case PingMessage =>
-        println("  pong")
-        sender() ! PongMessage
-    case StopMessage =>
-        println("pong stopped")
-        context.stop(self)
-  }
-}
-
-object PingPongTest extends App {
-  val system = ActorSystem("PingPongSystem")
-  val pong = system.actorOf(Props(new Pong), name = "pong")
-  val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
-  // start them going
-  ping ! StartMessage
+object VampusWorld extends App {
+  val system = ActorSystem("VampusWorld")
+  val navigator = system.actorOf(Props(new Navigator), name = "navigator")
+  val speleologist = system.actorOf(Props(new Speleologist(navigator)), name = "speleologist")
 }
