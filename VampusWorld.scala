3e842c8d3c12801405983e3e2c4b1899d425cd02
serhii.nastenko <serhii.nastenko@nentgroup.com>
2023-01-12 00:48:36 +0100

    Messages were added

--- VampusWorld.scala
+++ VampusWorld.scala
@@ -1,15 +1,24 @@
 import akka.actor._
 
-case object PingMessage
-case object PongMessage
-case object StartMessage
-case object StopMessage
+case object WhereAmIMessage
+case object WhatIShouldDoMessage
+case object Action
+case object LocationInfo
 
-class Speleologist(navigator: ActorRef) extends Actor {}
-class Navigator extends Actor {}
+
+class Speleologist(navigator: ActorRef, environment: ActorRef) extends Actor {
+  def receive = {}
+}
+class Navigator extends Actor {
+  def receive = {}
+}
+class Environment extends Actor {
+  def receive = {}
+}
 
 object VampusWorld extends App {
   val system = ActorSystem("VampusWorld")
   val navigator = system.actorOf(Props(new Navigator), name = "navigator")
-  val speleologist = system.actorOf(Props(new Speleologist(navigator)), name = "speleologist")
+  val environment = system.actorOf(Props(new Environment), name = "environment")
+  val speleologist = system.actorOf(Props(new Speleologist(navigator, environment)), name = "speleologist")
 }
